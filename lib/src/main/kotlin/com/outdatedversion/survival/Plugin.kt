3c24b4713f42fe52894e1f757e48fcf028546965
Ben Watkins <ben@outdatedversion.com>
2021-11-18 13:23:06 -0600

    unregister commands on disable

--- lib/src/main/kotlin/com/outdatedversion/survival/Plugin.kt
+++ lib/src/main/kotlin/com/outdatedversion/survival/Plugin.kt
@@ -1,5 +1,6 @@
 package com.outdatedversion.survival
 
+import co.aikar.commands.CommandManager
 import co.aikar.commands.PaperCommandManager
 import com.outdatedversion.survival.command.CoordinatesCommand
 import org.bukkit.plugin.java.JavaPlugin
@@ -8,11 +9,17 @@ import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 class Plugin: JavaPlugin() {
+    private lateinit var commandManager: PaperCommandManager
+
     override fun onEnable() {
-        val commandManager = PaperCommandManager(this)
-        commandManager.registerCommand(CoordinatesCommand())
+        this.commandManager = PaperCommandManager(this)
+        this.commandManager.registerCommand(CoordinatesCommand())
 
         val timeFormat = DateTimeFormatter.ofPattern("hh:mma v").withLocale(Locale.US).withZone(ZoneId.of("America/Chicago"))
         this.server.pluginManager.registerEvents(ChatMentionHandler(timeFormat), this);
+    }
+
+    override fun onDisable() {
+        this.commandManager.unregisterCommands()
     }
 }
