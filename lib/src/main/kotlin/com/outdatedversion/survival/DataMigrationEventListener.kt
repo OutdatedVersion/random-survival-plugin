e80293b4effde7b559060c90569af8f59ed4102d
Ben Watkins <ben@outdatedversion.com>
2021-12-27 19:13:48 -0600

    reconcile death scoreboard state on join

--- lib/src/main/kotlin/com/outdatedversion/survival/DataMigrationEventListener.kt
+++ lib/src/main/kotlin/com/outdatedversion/survival/DataMigrationEventListener.kt
@@ -2,6 +2,7 @@ package com.outdatedversion.survival
 
 import com.outdatedversion.survival.persistence.service.PointsOfInterestService
 import org.apache.logging.log4j.LogManager
+import org.bukkit.Statistic
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -19,5 +20,19 @@ class DataMigrationEventListener(private val poiService: PointsOfInterestService
                               this.logger.info("Added missing owner ID (${it.ownerId}) to ${it.id}")
                               this.poiService.save(it.ownerId!!, it)
                           }
+    }
+
+    @EventHandler
+    fun updateScoreboardDeathCount(event: PlayerJoinEvent) {
+        val player = event.player
+        val scoreboard = player.scoreboard
+        val obj = scoreboard.getObjective("Deaths") ?: return
+        val playerScoreboardEntry = obj.getScore(player.name)
+
+        val deathCount = player.getStatistic(Statistic.DEATHS)
+        if (playerScoreboardEntry.score != deathCount) {
+            playerScoreboardEntry.score = deathCount
+            this.logger.info("Updated death count to $deathCount for ${player.uniqueId}")
+        }
     }
 }
