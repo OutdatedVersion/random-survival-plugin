f71ebc4fc8f3143b51bcb926bf857f6ba0a92a83
Matt Hyme <matthyme@gmail.com>
2022-01-04 00:49:13 -0600

    removed space

--- scripts/extract_logs.py
+++ scripts/extract_logs.py
@@ -6,6 +6,6 @@ for fileName in os.listdir('logs'):
         continue
     with gzip.open(f"logs/{fileName}",'rb') as f_in:
         suffix = fileName.find(".gz")
-        with open(f"logs/{fileName[0:suffix]} ",'wb') as f_out:
+        with open(f"logs/{fileName[0:suffix]}",'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
         os.remove(f"logs/{fileName}")
