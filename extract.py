6143f15ba4ef28a4aed72267c63b19e1891ab60d
Matt Hyme <matthyme@gmail.com>
2022-01-03 23:48:43 -0600

    changed variable to be more readable

--- extract.py
+++ extract.py
@@ -5,7 +5,7 @@ for fileName in os.listdir('logs'):
     if not fileName.endswith('.gz'):
         continue
     with gzip.open("logs/" + fileName, 'rb') as f_in:
-        a = fileName.find(".gz")
-        with open("logs/" + fileName[0:a], 'wb') as f_out:
+        suffix = fileName.find(".gz")
+        with open("logs/" + fileName[0:suffix], 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
         os.remove("logs/" + fileName)
