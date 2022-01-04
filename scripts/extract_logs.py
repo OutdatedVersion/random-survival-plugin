8ba8bd2cac323cf6032f7cbda119de79fe171a52
Matt Hyme <matthyme@gmail.com>
2022-01-04 00:47:05 -0600

    fixed missing logic with f strings

--- scripts/extract_logs.py
+++ scripts/extract_logs.py
@@ -4,8 +4,8 @@ import os
 for fileName in os.listdir('logs'):
     if not fileName.endswith('.gz'):
         continue
-    with gzip.open(f"{fileName}",'rb') as f_in:
+    with gzip.open(f"logs/{fileName}",'rb') as f_in:
         suffix = fileName.find(".gz")
-        with open(f"{fileName}",'wb') as f_out:
+        with open(f"logs/{fileName[0:suffix]} ",'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
-        os.remove(f"{fileName}")
+        os.remove(f"logs/{fileName}")
